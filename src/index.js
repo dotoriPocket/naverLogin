990cbd9477ca35b261bb1a3e28067afce2fa5a97
dotoriPocket <monparis12@naver.com>
2023-03-09 19:28:47 +0900

    init project

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "../src/component/body/main.css";
+import btnText from './component/button/button';
+import inputBox from './component/input/input';
+import head from './component/body/logo';
+import main_box from './component/body/main';
+import check_box from "./component/input/check";
+
+function component() {
+    const element = document.createElement('div');
+
+    element.setAttribute("class", "body");
+
+    element.appendChild(head);
+    element.appendChild(main_box);
+    main_box.appendChild(inputBox("ID"));
+    main_box.appendChild(inputBox("PW"));
+    main_box.appendChild(check_box);
+    main_box.appendChild(btnText("로그인"));
+  
+    return element;
+  }
+  
+document.body.appendChild(component());
