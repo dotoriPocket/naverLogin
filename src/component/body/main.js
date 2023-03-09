a36f951c28342f37fc94434b8092923c556405c4
dotoriPocket <monparis12@naver.com>
2023-03-09 19:30:00 +0900

    add main box

---
+++ src/component/body/main.js
@@ -0,0 +1,10 @@
+import "./main.css";
+
+const main_box = document.createElement("div");
+const tap_box = document.createElement("div");
+
+main_box.setAttribute("class", "main_box");
+tap_box.setAttribute("class", "tap_box");
+
+
+export default main_box;
